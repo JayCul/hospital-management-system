30c3dcf5b322302c6f91afca92c6309b3b514dc2
JayCul <justincul01@gmail.com>
2025-05-07 06:12:30 +0100

    baseUrl Updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,13 @@
 // const apiBaseUrl= 'http://localhost:3000'; // Base URL for your backend
-const apiBaseUrl= 'https://medical-backend.zeabur.app'; // Base URL for your backend
+const apiBaseUrl = 'https://thick-vanda-jaystechub-9e5d8dcc.koyeb.app/'; // Base URL for your backend
 
 export const environment = {
-    production: false,
+  production: false,
 
-    authUrl: `${apiBaseUrl}/auth`,
-    userUrl: `${apiBaseUrl}/user`,
-    patientUrl: `${apiBaseUrl}/patient`,
-    drugUrl: `${apiBaseUrl}/drug`,
-    dashboardUrl: `${apiBaseUrl}/dashboard`,
-    prescriptionUrl: `${apiBaseUrl}/prescriptions`,
-
-  };
+  authUrl: `${apiBaseUrl}/auth`,
+  userUrl: `${apiBaseUrl}/user`,
+  patientUrl: `${apiBaseUrl}/patient`,
+  drugUrl: `${apiBaseUrl}/drug`,
+  dashboardUrl: `${apiBaseUrl}/dashboard`,
+  prescriptionUrl: `${apiBaseUrl}/prescriptions`,
+};
